6cdbd1aeb1afcc74c175937d7cce954f88d4eae5
Your Name <emekaihechi860@gmail.com>
2024-12-18 06:49:47 +0100

    #

---
+++ scr/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// Import the Home component from pages folder
+import Home from './pages/home';
+ 
+// Import the style component from pages folder
+import './styles/style.css';
+
+//Import the navbar component from pages folder
+import Navbar from './components/navbar';
+
+
+//Import the footer component from pages folder
+import Footer from './components/footer';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Nest Finder</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ scr/components/Navbar.jsx
@@ -0,0 +1,17 @@
+// src/components/Navbar.jsx
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/properties">Properties</a></li>
+        <li><a href="/about">About Us</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
